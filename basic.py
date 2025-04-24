79d53fc4ce6784442495e29d459244238aed5605
Waseem <waseemh.nyc@gmail.com>
2025-04-23 21:49:31 -0400

    Update basic.py

--- basic.py
+++ basic.py
@@ -6,14 +6,13 @@ from geometry_msgs.msg import Twist
 class TurtlePublisher(Node):
     def __init__(self):
         super().__init__('turtle_vel_publisher')
-        self.vcat_publisher = self.create_publisher(Twist, '/cmd_vel', 10)
-        self.timer = self.create_timer(0.5, self.run_now)
+        self.my_publisher = self.create_publisher(Twist, '/cmd_vel', 10)
 
     def run_now(self):
         msg = Twist()
         msg.linear.x = 5.0
         msg.angular.z = 0.0
-        self.vcat_publisher.publish(msg)
+        self.my_publisher.publish(msg)
         print(f'Published: linear.x={msg.linear.x}, angular.z={msg.angular.z}')
 
 def main():
